ba143981a8e67ec001b5ddbf9467b190bdfc5d9b
ceske-42 <ceske@etik.com>
2025-11-04 12:48:47 +0100

    Added ft_realloc function to library

--- ft_lib/ft_lib.h
+++ ft_lib/ft_lib.h
@@ -38,6 +38,7 @@ void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_calloc(size_t nmemb, size_t size);
+void				*ft_realloc(void *ptr, size_t old_size, size_t new_size);
 
 /*FT_STR*/
 int					ft_strlen(const char *s);
