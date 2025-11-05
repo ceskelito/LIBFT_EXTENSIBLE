80a7dd747d1d29d3a75332b7ee6a02a47bdd8a26
ceske-42 <ceske@etik.com>
2025-11-05 11:49:20 +0100

    Norminetted realloc

--- ft_lib/ft_mem/ft_realloc.c
+++ ft_lib/ft_mem/ft_realloc.c
@@ -3,42 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_realloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rceschel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rceschel <rceschel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 12:46:25 by rceschel          #+#    #+#             */
-/*   Updated: 2025/11/04 12:46:28 by rceschel         ###   ########.fr       */
+/*   Updated: 2025/11/05 11:48:21 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "../ft_lib.h" // ft_memcpy
+#include <stdlib.h>
 
-void *ft_realloc(void *ptr, size_t old_size, size_t new_size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-    void *new;
+	void	*new;
 
-    /* Caso 1: nuova dimensione zero → libera e ritorna NULL */
-    if (new_size == 0)
-    {
-        free(ptr);
-        return NULL;
-    }
-
-    /* Caso 2: ptr è NULL → si comporta come malloc() */
-    if (ptr == NULL)
-        return malloc(new_size);
-
-    /* Caso 3: normale riallocazione */
-    new = malloc(new_size);
-    if (new == NULL)
-        return NULL;
-
-    /* Copia solo i byte che esistono in entrambe le allocazioni */
-    if (old_size < new_size)
-        ft_memcpy(new, ptr, old_size);
-    else
-        ft_memcpy(new, ptr, new_size);
-
-    free(ptr);
-    return new;
+	if (new_size == 0)
+	{
+		free(ptr);
+		ptr = NULL;
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
+	if (old_size < new_size)
+		ft_memcpy(new, ptr, old_size);
+	else
+		ft_memcpy(new, ptr, new_size);
+	free(ptr);
+	return (new);
 }
